0e89eb1a1b1b2b61294f1c09d80f43c51fd4da81
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-12-03 20:25:41 -0500

    Add opacity to buttons on cover

--- components/cover.tsx
+++ components/cover.tsx
@@ -41,7 +41,7 @@ export const Cover = ({ url, preview }: CoverProps) => {
     >
       {!!url && <Image src={url} fill alt="Cover" className="object-cover" />}
       {url && !preview && (
-        <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+        <div className="opacity-0 group-hover:opacity-100 transition-opacity absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
             onClick={() => coverImage.onReplace(url)}
             className="text-muted-foreground text-xs"
