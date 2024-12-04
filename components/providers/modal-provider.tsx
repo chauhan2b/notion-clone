23eea51ec103c93d236dadcbfe9664efec226180
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-12-03 20:22:12 -0500

    Allow to add, replace and remove cover image

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useEffect, useState } from "react";
+
 import { SettingsModal } from "../modals/settings-modal";
+import { CoverImageModal } from "../modals/cover-image-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -17,6 +19,7 @@ export const ModalProvider = () => {
   return (
     <>
       <SettingsModal />
+      <CoverImageModal />
     </>
   );
 };
